05276098fce61273bb9dbd640b11d1946c07cf11
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-11-28 14:43:22 +0100

    Add CSS minification.
    Use `grunt-contrib-cssmin` and `grunt-contrib-clean` to implement
    CSS minification. The latter is used to clean the assembled CSS
    file produced by UnCSS.
    
    Fixes #4.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,17 @@
 module.exports = function (grunt) {
   grunt.initConfig({
+    clean: {
+      dist: ['dist/css/application.css']
+    },
+
+    cssmin: {
+      dist: {
+        files: {
+          'dist/css/application.min.css': ['dist/css/application.css']
+        }
+      }
+    },
+
     processhtml: {
       dist: {
         files: {
@@ -9,24 +21,33 @@ module.exports = function (grunt) {
     },
 
     uglify: {
-      build: {
-        src: 'js/*.js',
-        dest: 'dist/js/application.min.js'
+      dist: {
+        files: {
+          'dist/js/application.min.js': ['js/*.js']
+        }
       }
     },
 
     uncss: {
       dist: {
         files: {
-          'dist/css/application.min.css': ['index.html']
+          'dist/css/application.css': ['index.html']
         }
       }
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-processhtml');
   grunt.loadNpmTasks('grunt-uncss');
 
-  grunt.registerTask('default', ['uglify', 'uncss', 'processhtml']);
+  grunt.registerTask('default', [
+    'uglify',
+    'uncss',
+    'cssmin',
+    'processhtml',
+    'clean'
+  ]);
 };
